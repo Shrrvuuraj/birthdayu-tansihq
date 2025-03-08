54df4efb1b580e9e9fe0f87d540fc7fe5a5becb7
Shravan Rajput <shrrvvuurajput@gmail.com>
2025-03-09 02:19:49 +0530

    sound

--- index.js
+++ index.js
@@ -161,61 +161,25 @@ function startConfetti() {
 }
 
 
+let ayeein=new Audio()
+ayeein.src=`assests/ayeein.m4a`
 
 
-
-
-
-
-
-
-// const audioPlayer = (() => {
-//     let clickCount = 0;
-//     let totalDuration = 0;
-//     const audio = new Audio();
-//     audio.src = "assests/yo.m4a"; // ← Your audio path here
-//     audio.type = "audio/mpeg";
+let baigan=new Audio()
+baigan.src=`assests/baigan.m4a`
+let cou=0;
+btn.addEventListener("click",()=>{
     
-    
-//     // Initialize audio duration once loaded
-//     audio.addEventListener('loadedmetadata', () => {
-//         totalDuration = audio.duration;
-//     });
+    if (cou==0){
+        ayeein.play()
+        cou=1;
+    }else(
+        cou=0,
+        baigan.play())
+   
+})
 
-//     return () => {
-//         clickCount++;
-        
-//         if (clickCount <= 6) {
-//             const segment = totalDuration / 6;
-//             const startTime = (clickCount - 1) * segment;
-//             const endTime = clickCount * segment;
 
-//             audio.currentTime = startTime;
-//             audio.play();
 
-//             // Auto-pause at segment end
-//             const stopAtEnd = () => {
-//                 if (audio.currentTime >= endTime || 
-//                    (clickCount === 6 && audio.currentTime === totalDuration)) {
-//                     audio.pause();
-//                     audio.removeEventListener('timeupdate', stopAtEnd);
-//                 }
-//             };
-            
-//             audio.addEventListener('timeupdate', stopAtEnd);
-//         }
 
-//         // Reset after 6 clicks
-//         if (clickCount === 6) {
-//             setTimeout(() => {
-//                 clickCount = 0;
-//                 audio.currentTime = 0;
-//             }, 500);
-//         }
-//     };
-// })();
 
-// // Assign to global scope for button click
-// function playAudio() {
-//     audioPlayer();
-// }
